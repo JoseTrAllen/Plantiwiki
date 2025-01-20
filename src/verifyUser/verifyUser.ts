c0b38ae4199b5d2336f8120b5baf67d61375840b
JoseTA <josetrenor@gmail.com>
2025-01-20 15:03:43 +0100

    Copia del proyecto original con las rutas reemplazadas correctamente para despliegue

---
+++ src/verifyUser/verifyUser.ts
@@ -0,0 +1,20 @@
+import { fetchUsers } from "../api/usersApi.ts";
+import { User } from "../model/model.ts";
+
+export const verifyUser = async (user: string, password: string) => {
+  const users: User[] = await fetchUsers();
+  const isValid = users.find(
+    (u) => u.email === user && u.password === password
+  );
+
+  if (isValid) {
+    console.log("Usuario correcto");
+    console.log(isValid.email, isValid.password);
+
+    window.location.href = `/userProfile.html?username=${isValid.username}`;
+  } else {
+    console.log(user, password);
+
+    console.log("Usuario o contraseña incorrecta");
+  }
+};
